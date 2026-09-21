72a06c99f76ee5d837b4d497c2e0c020d9721b58
Metin Turan <turan.ahmetmetin0@gmail.com>
2026-09-21 23:00:42 +0300

    Create day1.sql

---
+++ sql/sql-practice/day1.sql
@@ -0,0 +1,33 @@
+-- Query 1
+SELECT *
+FROM job_postings_fact
+WHERE job_title_short = 'Data Analyst';
+
+-- Query 2
+SELECT
+    job_title_short,
+    job_via,
+    salary_year_avg
+FROM job_postings_fact
+WHERE salary_year_avg >= 0;
+
+-- Query 3
+SELECT
+    job_title_short,
+    job_location,
+    job_via,
+    salary_year_avg
+FROM job_postings_fact
+WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg > 90000
+ORDER BY salary_year_avg ASC;
+
+-- Query 4
+SELECT
+    job_title_short,
+    job_location,
+    job_via,
+    salary_year_avg
+FROM job_postings_fact
+WHERE job_title_short = 'Data Analyst'
+    AND job_location IN ('Argentina', 'Turkey', 'Canada');
